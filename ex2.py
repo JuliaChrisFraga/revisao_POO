5ebf93f26c231ee1f762b2b31b0834186ad7c214
JuliaChrisFraga <christina.brito@aluno.edu.br>
2025-04-01 15:59:48 -0300

    Exercício 2: versão 1

---
+++ ex2.py
@@ -0,0 +1,24 @@
+class Domino():
+    def __init__(self, a=0, b=0):
+        self.a = a
+        self.b = b
+    
+    def mostrar_pontos(self):
+        mensagem = f"Lado A: {self.a}; Lado B: {self.b}."
+        return mensagem
+    
+    def valor(self):
+        soma = self.a + self.b
+        mensagem = f"Total de pontos: {soma}"
+        return mensagem
+    
+    def  __str__(self):
+        return f'({self.a},{self.b})'
+
+d1=Domino(2,6)
+d2=Domino(4,3)
+print(d1.mostrar_pontos())
+print(d2.mostrar_pontos())
+print(d1.valor())
+print(d2.valor())
+print(d1.__str__())

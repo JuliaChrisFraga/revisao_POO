04cd23d41d036f35f697c6eeb1a568f848f351f8
JuliaChrisFraga <christina.brito@aluno.edu.br>
2025-04-01 15:42:34 -0300

    ex1 v1.0

---
+++ ex1.py
@@ -0,0 +1,29 @@
+import math
+
+class Circle:
+    def __init__(self, a, b, r):
+        self.O = (a, b)
+        self.r = r
+    
+    def area(self):
+        area = math.pi * math.pow(self.r, 2)
+        return area
+    
+    def perimetro(self):
+        perimetro = 2 * math.pi * self.r
+        return perimetro
+    
+    def test_pertencente(self, x, y):
+        A = (x,y)
+        distancia = math.sqrt(math.pow(A[0] - self.O[0], 2) + math.pow(A[1]- self.O[1], 2))  # Distância entre (x, y) e O
+        if (distancia <= self.r):
+            return True
+        else:
+            return False 
+
+c1= Circle(2,2,3)
+print()
+print(c1)
+print(c1.test_pertencente(0,0))
+print(c1.test_pertencente(0,-1))
+        

879cdb57f656eb3d612e012e604711a43c1db488
JuliaChrisFraga <christina.brito@aluno.ifsp.edu.br>
2025-04-02 16:24:38 -0300

    Exercício 4: versão 2

--- ex4.py
+++ ex4.py
@@ -4,18 +4,56 @@ class ContaBancaria():
         self.__titular = __titular
     
     def depositar(self, valor):
-        self.saldo = self.saldo + valor
-        return self.saldo
+        self.__saldo = self.__saldo + valor
+        return self.__saldo
   
     def sacar(self, valor):
-        if (self.saldo > valor):
-            self.saldo = self.saldo + valor
-            return self.saldo
+        if (self.__saldo > valor):
+            self.__saldo = self.__saldo - valor
+            return self.__saldo
         else:
             mensagem = "Saldo insuficiente."
             return mensagem
+    
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @property
+    def titular(self):
+        return self.__titular
+    
+    @saldo.setter
+    def saldo(self, valor):
+        try:
+            valor = float(valor)
+            if (valor >= 0):
+                self.__saldo = valor
+            else:
+                print("Valor inválido. O saldo não pode ser negativo.")
+        except ValueError:
+            print("Valor inválido. Insira um número válido.")
+    
+    @titular.setter
+    def titular(self, new):
+        if not new:
+            print("Não é permitido nomes vazios. tente novamente.")
+        else:
+            self.__titular = new
 
-    def exibirSaldo(self):
-        return self.saldo
+p1 = ContaBancaria(2000, "zaoba")
+
+print("Exibindo o titular:", p1.titular, "\n") #se não tivesse o property, seria p1.exibirTitular()
+print("Exibindo o saldo atual:", p1.saldo) #se não tivesse o property, seria p1.exibirSaldo()
+print("Depositando 200:", p1.depositar(200))
+print("Sacando 100:", p1.sacar(100))
+
+p1.saldo = "abc" #sem o setter, chamaria-se a função e passaria-se como parâmetro o Valor. Also, o setter tem que ter o mesmo nome do seu respectivo get
+p1.saldo = "400"
+print("Saldo depois de alterado: ", p1.saldo, "\n")
+
+p1.titular = ""
+p1.titular = "amaba"
+print("Titular depois de alterado: ", p1.titular)
 
     
